8986f9b91af28aec05b527f9bc3b9ee08519282f
Vishal Sharma <vsh1819@gmail.com>
2018-08-04 06:02:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function deleteFromObjectByKey(object, key) {
 
 function deleteFromObjectByKey(object, key) {
   var newObj = Object.assign({}, object)
-  
+
   delete newObj.key
   return object
 }
