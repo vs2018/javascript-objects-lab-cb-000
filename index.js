3bc05e86535973ecf539cffb464c04e7de9ba76f
Vishal Sharma <vsh1819@gmail.com>
2018-08-04 05:43:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,6 +10,6 @@ function updateObjectWithKeyAndValue(object, key, value) {
 }
 
 
-function updateObjectWithKeyAndValue(object, key, value) {
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   return Object.assign({}, object, { [key]: value })
 }
