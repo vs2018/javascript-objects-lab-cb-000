7dd6f02a0cc23bffcc9ab408daabd73eb5ee3b85
Vishal Sharma <vsh1819@gmail.com>
2018-08-04 05:56:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,3 +19,10 @@ function deleteFromObjectByKey(object, key) {
   delete object[key]
   return object
 }
+
+function deleteFromObjectByKey(object, key) {
+  delete object.key
+  return object
+}
+
+
